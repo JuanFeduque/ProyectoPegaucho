1e3ea2b5cf9b764c6557ace9afa4785408da3dd4
JuanFeduque <133066813+JuanFeduque@users.noreply.github.com>
2025-10-13 16:23:56 -0500

    Ultimas modificaciones

---
+++ Pegaucho.Shared/Entities/Produccion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pegaucho.Shared.Entities
+{
+    public class Produccion
+    {
+        [Key]
+        public int idLogin { get; set; }
+
+
+        /*[Required(ErrorMessage = "El campo es obligatorio")]*/
+        /*[MaxLength(100, ErrorMessage = "El campo no puede tener mas caracteres")]*/
+        public string usuario { get; set; } = null!;
+        public string contrasena { get; set; } = null!;
+    }
+}
---
+++ PegauchoBackend/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entities;
+
+namespace PegauchoBackend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        //validaciones
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Usuario>().HasIndex(x => x.usuario).IsUnique();
+        }   */
+    }
+
+}
